27ab56da26741f4ddbe9858279d40587e0bd3a09
Boysen <oldboysen@gmail.com>
2026-05-28 21:36:44 +0300

    go variables

--- main.go
+++ main.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("hello, Go!")
+func main() {
+
+	// var age int = 25
+	// fmt.Println(age)
+
+	//VARIABLE AND TYPES
+	name := "John"
+	age := 25
+
+	name = "Laurent"
+
+	isOpen := true
+
+	fmt.Println(name, age, isOpen)
+
+	var a string
+	var b int
+	var c bool
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
 }
